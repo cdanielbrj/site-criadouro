5f5632792901f35cda5bafd036f62e8ad65d72ff
Daniel Carvalho <danielcarvalho726@gmail.com>
2024-06-16 22:20:13 -0300

    Relacionamento de aves concluída (ID)

--- single-aves.php
+++ single-aves.php
@@ -11,9 +11,63 @@ if ( have_posts() ) :
                 <?php if ( function_exists('get_field') ) : ?>
                     <p><strong>Nome:</strong> <?php the_field('ave_nome'); ?></p>
                     <p><strong>Nome Científico:</strong> <?php the_field('ave_nome_cientifico'); ?></p>
+					<p><strong>Nome Popular:</strong> <?php the_field('ave_popular'); ?></p>
                     <p><strong>Data de Nascimento:</strong> <?php the_field('ave_nascimento'); ?></p>
-                    <p><strong>Ave Pai:</strong> <?php the_field('ave_pai'); ?></p>
-                    <p><strong>Ave Mãe:</strong> <?php the_field('ave_mae'); ?></p>
+					<p><strong>Sexo:</strong> <?php the_field('ave_sexo'); ?></p>
+                   	<!-- Ave Pai -->
+					<p><strong>Ave Pai:</strong> 
+						<?php
+						$ave_pai_id = get_field('ave_pai');
+						if ($ave_pai_id) {
+							if (is_array($ave_pai_id)) {
+								$nomes_pais = array();
+								foreach ($ave_pai_id as $pai_id) {
+									$nome_pai = get_field('ave_nome', $pai_id);
+									if ($nome_pai) {
+										$nomes_pais[] = $nome_pai;
+									}
+								}
+								if (!empty($nomes_pais)) {
+									echo implode(', ', $nomes_pais);
+								}
+							} else {
+								$ave_pai_nome = get_field('ave_nome', $ave_pai_id); // Obtém o nome da ave pai
+								if ($ave_pai_nome) {
+									echo $ave_pai_nome;
+								}
+							}
+						} else {
+							echo 'Sem pai';
+						}
+						?>
+					</p>
+					<!-- Ave Mãe -->
+					<p><strong>Ave Mãe:</strong> 
+					<?php
+					$ave_mae_id = get_field('ave_mae');
+					if ($ave_mae_id) {
+						if (is_array($ave_mae_id)) {
+							$nomes_maes = array();
+							foreach ($ave_mae_id as $mae_id) {
+								$nome_mae = get_field('ave_nome', $mae_id);
+								if ($nome_mae) {
+									$nomes_maes[] = $nome_mae;
+								}
+							}
+							if (!empty($nomes_maes)) {
+								echo implode(', ', $nomes_maes);
+							}
+						} else {
+							$ave_mae_nome = get_field('ave_nome', $ave_mae_id); // Obtém o nome da ave mãe
+							if ($ave_mae_nome) {
+								echo $ave_mae_nome;
+							}
+						}
+					} else {
+						echo 'Sem mãe';
+					}
+					?>
+					</p>
                     <p><strong>Cor:</strong> <?php the_field('ave_cor'); ?></p>
                 <?php endif; ?>
             </div>
